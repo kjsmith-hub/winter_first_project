5c23c8e85adf5c6506f2b4168b45f02563e1475b
kjsmith-hub <keaton.smith@gmail.com>
2019-12-30 13:49:18 -0800

    first day added

---
+++ main 2.py
@@ -0,0 +1,43 @@
+# Exercise one
+# name = input("what is your name?")
+# print("hello {}".format(name))
+
+# exercise 2
+
+# x = 10
+# print(x)
+
+# exercise 3
+# get user to provide number
+# tell user whether that number is:
+# - smaller
+# - larger
+# - or equal 10
+
+# num = int(input("enter an number:"))
+# if num > 10:
+#     print("the number is greater than 10")
+# elif num < 10:
+#     print("the number is smaller than 10")
+# else:
+#    print("the number is equal to 10")
+
+# exercise 4: arrays
+
+# my_var = 5
+# my_array = ["name", 123, my_var]
+# for e in my_array:
+#     print(e)
+
+#
+# my_list =[040404, "waffles", "donuts"]
+# my_list.sort()
+# my_list.append('watermelon')
+# print(my_list)
+
+import random
+
+num_array = [ ]
+for i in range [ 10 ]
+    num_array.append ( random.randint ( 1 , 100 ) )
+print(num_array)
